be1c414d10caadf9fbbb610abf0312378eab2a35
Jake Browning <jake@datapane.com>
2020-10-05 18:06:59 +0100

    For in -> for of

--- index.js
+++ index.js
@@ -11,21 +11,18 @@ async function run() {
 
         !requirements.includes("datapane") && requirements.push("datapane");
 
-        core.info("Requirements::");
-        core.info(requirements);
+        for (const requirement of requirements) {
+            core.info(`installing ${requirement}`);
+            await exec(`pip install ${requirement}`);
+        }
 
-        // for (const requirement in requirements) {
-        //     core.info(`installing ${requirement}`);
-        //     await exec(`pip install ${requirement}`);
-        // }
-        //
-        // if (token) {
-        //     core.info(`Logging in to ${server}`);
-        //     await exec(`datapane login --token=${token} --server=${server}`);
-        // }
-        //
-        // core.info(`Executing ${scriptPath}`);
-        // await exec(`python ${scriptPath}`);
+        if (token) {
+            core.info(`Logging in to ${server}`);
+            await exec(`datapane login --token=${token} --server=${server}`);
+        }
+
+        core.info(`Executing ${scriptPath}`);
+        await exec(`python ${scriptPath}`);
 
     } catch (error) {
         core.setFailed(error.message);
