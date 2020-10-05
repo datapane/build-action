fa41d6cf78c8a04babc6fbbed21e7ea7dc2fbe0f
Jake Browning <jake@datapane.com>
2020-10-05 16:28:25 +0100

    WIP get inputs from @actions/core

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
+
+try {
+    const scriptPath = core.getInput("path");
+    const token = core.getInput("token");
+    const requirements = core.getInput("requirements");
+    const params = core.getInput("parameters");
+    core.setOutput("summary",  token);
+} catch (error) {
+    core.setFailed(error.message);
+}
