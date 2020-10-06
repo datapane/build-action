f9cc46b8a8c4e1641f7ac2a90909160234d16057
Jake Browning <jake@datapane.com>
2020-10-06 16:16:13 +0100

    Remove log

--- index.js
+++ index.js
@@ -23,14 +23,8 @@ async function run() {
 
         const paramsExec = Object.entries(params).map(([k, v]) => `--parameter ${k}=${v}`).join(" ");
 
-        core.info("PARAMS::");
-        core.info(core.getInput("parameters"));
-        core.info(params);
-        core.info(paramsExec);
-        core.info("----");
-
-        // core.info(`Executing ${fullScriptPath}`);
-        // await exec(`python ${fullScriptPath} ${paramsExec}`);
+        core.info(`Executing ${fullScriptPath}`);
+        await exec(`python ${fullScriptPath} ${paramsExec}`);
 
     } catch (error) {
         core.setFailed(error.message);
