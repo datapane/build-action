da9c65045940575df6e5a4b45b5e31b203052dd9
Jake Browning <jake@datapane.com>
2020-10-06 10:33:59 +0100

    Update dp_script

---
+++ tests/dp_script.py
@@ -0,0 +1,18 @@
+"""datapane script"""
+import pandas as pd
+import datapane as dp
+
+
+def gen_df(dim: int = 4) -> pd.DataFrame:
+    axis = [i for i in range(0, dim)]
+    data = {"x": axis, "y": axis}
+    return pd.DataFrame.from_dict(data)
+
+# Report
+report = dp.Report(
+    "## Sample report",
+    dp.Table(gen_df(10000), can_pivot=False)
+)
+
+report.publish(name="layout_report")
+# report.save(path="local_layout_report.html")
