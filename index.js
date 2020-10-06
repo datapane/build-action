9144ec47ab88e8e6e12da87343134b8eb6d58154
Jake Browning <jake@datapane.com>
2020-10-06 15:59:24 +0100

    Pass params to script exec line

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ async function run() {
         const scriptPath = core.getInput("script_path");
         const token = core.getInput("token");
         const requirements = JSON.parse(core.getInput("requirements"));
-        const params = core.getInput("parameters");
+        const params = JSON.parse(core.getInput("parameters"));
         const server = core.getInput("server");
         const fullScriptPath = path.join(process.env.GITHUB_WORKSPACE, scriptPath);
 
         !requirements.includes("datapane") && requirements.push("datapane");
 
-        for (const requirement of requirements) {
-            core.info(`installing ${requirement}`);
-            await exec(`pip install ${requirement}`);
-        }
+        core.info("Installing dependencies");
+        await exec(`pip install ${requirements.join(" ")}`);
 
         if (token) {
             core.info(`Logging in to ${server}`);
             await exec(`datapane login --token=${token} --server=${server}`);
         }
 
+        const paramsExec = Object.entries(params).map(([k, v]) => `--parameter ${k}=${v}`).join(" ");
+
         core.info(`Executing ${fullScriptPath}`);
-        await exec(`python ${fullScriptPath}`);
+        await exec(`python ${fullScriptPath} ${paramsExec}`);
 
     } catch (error) {
         core.setFailed(error.message);
